601a09da9b419482736fec6b1e72601e2fe7478b
Barbara Pakula <barbara.pakula@hotmail.com>
2021-01-11 14:00:27 +0100

    final game

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 function playGame(playerInput) {
 	clearMessages();
+
 	function getMoveName(argMoveId) {
-		if(argMoveId == 1) {
+		if (argMoveId == 1) {
 			return 'kamień';
 		} else
-		if(argMoveId == 2) {
+		if (argMoveId == 2) {
 			return 'papier';
 		} else
-		if(argMoveId == 3) {
+		if (argMoveId == 3) {
 			return 'nożyce';
 		}
 		printMessage('Nie znam ruchu o id ' + argMoveId + '.');
@@ -15,7 +16,7 @@ function playGame(playerInput) {
 	}
 	// computerMove
 	let randomNumber = Math.floor(Math.random() * 3 + 1);
-	console.log('Wylosowana liczba to: ' + randomNumber);
+	// console.log('Wylosowana liczba to: ' + randomNumber);
 	let argComputerMove = getMoveName(randomNumber);
 	printMessage('Mój ruch to: ' + argComputerMove);
 	//playerMove
@@ -24,22 +25,22 @@ function playGame(playerInput) {
 	printMessage('Twój ruch to: ' + argPlayerMove);
 	// result
 	function displayResult(argComputerMove, argPlayerMove) {
-		console.log('moves:', argComputerMove, argPlayerMove);
+		// console.log('moves:', argComputerMove, argPlayerMove);
 		printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-		if(argComputerMove == argPlayerMove) {
+		if (argComputerMove == argPlayerMove) {
 			printMessage('Remis!');
 		} else
-		if((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
+		if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
 			printMessage('Brawo! Ty wygrywasz');
-			 scoreuser++;
-			 scoreuserDisplay.textContent=scoreuser;
-		// }else
-		// if(argPlayerMove == 'nieznany ruch') {
-		// 	printMessage('Grasz nieuczciwie. Stawiasz piwo i dam Ci jeszcze szanse');
+			scoreuser++;
+			scoreuserDisplay.textContent = scoreuser;
+			// }else
+			// if(argPlayerMove == 'nieznany ruch') {
+			// 	printMessage('Grasz nieuczciwie. Stawiasz piwo i dam Ci jeszcze szanse');
 		} else {
 			printMessage('Przykro mi. Przegrałeś');
 			scorecomputer++;
-			scorecomputerDisplay.textContent=scorecomputer;
+			scorecomputerDisplay.textContent = scorecomputer;
 		}
 	}
 	displayResult(argComputerMove, argPlayerMove);
@@ -55,6 +56,6 @@ document.getElementById('play-scissors').addEventListener('click', function() {
 });
 
 let scoreuserDisplay = document.getElementById('scoreuser');
-	let scorecomputerDisplay = document.getElementById('scorecomputer');
-	let scoreuser = 0;
-	let scorecomputer = 0;
+let scorecomputerDisplay = document.getElementById('scorecomputer');
+let scoreuser = 0;
+let scorecomputer = 0;
