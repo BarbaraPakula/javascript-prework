d67e00e32ff9cf17efe6226995cebb83738c8cfe
Barbara Pakula <barbara.pakula@hotmail.com>
2021-01-12 10:45:42 +0100

    cleaning js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,17 @@ function playGame(playerInput) {
 		if (argMoveId == 1) {
 			return 'kamień';
 		} else
-		if (argMoveId == 2) {
-			return 'papier';
-		} else
-		if (argMoveId == 3) {
-			return 'nożyce';
-		}
+			if (argMoveId == 2) {
+				return 'papier';
+			} else
+				if (argMoveId == 3) {
+					return 'nożyce';
+				}
 		printMessage('Nie znam ruchu o id ' + argMoveId + '.');
 		return 'nieznany ruch';
 	}
 	// computerMove
 	let randomNumber = Math.floor(Math.random() * 3 + 1);
-	// console.log('Wylosowana liczba to: ' + randomNumber);
 	let argComputerMove = getMoveName(randomNumber);
 	printMessage('Mój ruch to: ' + argComputerMove);
 	//playerMove
@@ -25,18 +24,13 @@ function playGame(playerInput) {
 	printMessage('Twój ruch to: ' + argPlayerMove);
 	// result
 	function displayResult(argComputerMove, argPlayerMove) {
-		// console.log('moves:', argComputerMove, argPlayerMove);
 		printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 		if (argComputerMove == argPlayerMove) {
 			printMessage('Remis!');
-		} else
-		if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
+		} else if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
 			printMessage('Brawo! Ty wygrywasz');
 			scoreuser++;
 			scoreuserDisplay.textContent = scoreuser;
-			// }else
-			// if(argPlayerMove == 'nieznany ruch') {
-			// 	printMessage('Grasz nieuczciwie. Stawiasz piwo i dam Ci jeszcze szanse');
 		} else {
 			printMessage('Przykro mi. Przegrałeś');
 			scorecomputer++;
@@ -45,13 +39,13 @@ function playGame(playerInput) {
 	}
 	displayResult(argComputerMove, argPlayerMove);
 }
-document.getElementById('play-rock').addEventListener('click', function() {
+document.getElementById('play-rock').addEventListener('click', function () {
 	playGame(1);
 });
-document.getElementById('play-paper').addEventListener('click', function() {
+document.getElementById('play-paper').addEventListener('click', function () {
 	playGame(2);
 });
-document.getElementById('play-scissors').addEventListener('click', function() {
+document.getElementById('play-scissors').addEventListener('click', function () {
 	playGame(3);
 });
 
@@ -59,3 +53,11 @@ let scoreuserDisplay = document.getElementById('scoreuser');
 let scorecomputerDisplay = document.getElementById('scorecomputer');
 let scoreuser = 0;
 let scorecomputer = 0;
+
+// pętla do rozegrania 1000 gier
+
+// for (let i = 0; i <= 1000; i++) {
+// 	let randomNumber = Math.floor(Math.random() * 3 + 1);
+// 	playGame(randomNumber);
+
+// }
