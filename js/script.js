c67d847a6de57224a2d80bc2f730e57f959a3e96
Barbara Pakula <barbara.pakula@hotmail.com>
2021-01-13 17:14:29 +0100

    changed variables and function from let to const

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-function playGame(playerInput) {
-	clearMessages();
+let scoreuser = 0,
+	scorecomputer = 0;
+const scoreuserDisplay = document.getElementById('scoreuser'),
+	scorecomputerDisplay = document.getElementById('scorecomputer');
+document.getElementById('play-rock').addEventListener('click', function () {
+	playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function () {
+	playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function () {
+	playGame(3);
+});
 
-	function getMoveName(argMoveId) {
+const playGame = function (playerInput) {
+	const getMoveName = function (argMoveId) {
 		if (argMoveId == 1) {
 			return 'kamień';
-		} else
-			if (argMoveId == 2) {
-				return 'papier';
-			} else
-				if (argMoveId == 3) {
-					return 'nożyce';
-				}
-		printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-		return 'nieznany ruch';
-	}
-	// computerMove
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
-	let argComputerMove = getMoveName(randomNumber);
-	printMessage('Mój ruch to: ' + argComputerMove);
-	//playerMove
-	console.log('Gracz wpisał: ' + playerInput);
-	let argPlayerMove = getMoveName(playerInput);
-	printMessage('Twój ruch to: ' + argPlayerMove);
-	// result
-	function displayResult(argComputerMove, argPlayerMove) {
+		} else if (argMoveId == 2) {
+			return 'papier';
+		} else if (argMoveId == 3) {
+			return 'nożyce';
+		}
+	};
+	const randomNumber = Math.floor(Math.random() * 3 + 1),
+		argComputerMove = getMoveName(randomNumber);
+	const argPlayerMove = getMoveName(playerInput);
+
+	const displayResult = function (argComputerMove, argPlayerMove) {
 		printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 		if (argComputerMove == argPlayerMove) {
 			printMessage('Remis!');
@@ -36,28 +39,13 @@ function playGame(playerInput) {
 			scorecomputer++;
 			scorecomputerDisplay.textContent = scorecomputer;
 		}
-	}
+	};
+	clearMessages();
 	displayResult(argComputerMove, argPlayerMove);
-}
-document.getElementById('play-rock').addEventListener('click', function () {
-	playGame(1);
-});
-document.getElementById('play-paper').addEventListener('click', function () {
-	playGame(2);
-});
-document.getElementById('play-scissors').addEventListener('click', function () {
-	playGame(3);
-});
+};
 
-let scoreuserDisplay = document.getElementById('scoreuser');
-let scorecomputerDisplay = document.getElementById('scorecomputer');
-let scoreuser = 0;
-let scorecomputer = 0;
 
 // pętla do rozegrania 1000 gier
 
 // for (let i = 0; i <= 1000; i++) {
-// 	let randomNumber = Math.floor(Math.random() * 3 + 1);
-// 	playGame(randomNumber);
-
-// }
+// 	let r
